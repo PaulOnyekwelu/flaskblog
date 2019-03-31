4a3276e75d2062e36ebe931f0c8bcd381d969dbf
silanka007 <silanka007@gmail.com>
2019-03-31 17:06:08 +0100

    category section refactored

--- app/admin_views.py
+++ app/admin_views.py
@@ -5,10 +5,36 @@ from flask import render_template, url_for
 @app.route('/admin/dashboard')
 def dashboard():
     title = 'Admin Dashboard'
-
     return render_template('admin/dashboard.html', title = title)
 
 @app.route('/admin/profile')
 def profile():
     title = 'Admin Profile'
     return render_template('admin/profile.html', title = title)
+
+@app.route('/admin/register', methods=['GET', 'POST'])
+def register():
+    title = 'Registration Page'
+    return render_template('admin/hide/register.html', title = title)
+
+@app.route('/admin/new-post')
+def newPost():
+    return render_template('admin/new_post.html')
+
+@app.route('/admin/view-post')
+def viewPost():
+    return render_template('admin/view_post.html')
+
+@app.route('/admin/category')
+def category():
+    return render_template('admin/category.html')
+
+@app.route('/admin/comment')
+def comment():
+    return render_template('admin/comment.html')
+
+@app.route('/admin/settings')
+def siteSetting():
+    return render_template('admin/site_setting.html')
+
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, url_for
-from .process.forms import UserLoginForm, UserRegistrationForm
+
 
 app.config['SECRET_KEY'] = 'taoPSJfjPOjfkfsfD'
 #front page routing
@@ -17,11 +17,9 @@ def about():
 @app.route('/login', methods=['GET','POST'])
 def login():
     title = 'Login Page'
-    user_login = UserLoginForm()
-    return render_template('public/login.html', login = user_login, title = title)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    title = 'Registration Page'
-    user_register  = UserRegistrationForm()
-    return render_template('public/register.html', register = user_register, title = title)
+    return render_template('public/login.html', title = title)
+    
+@app.route('/reset-password')
+def forgot():
+    title = 'Password Reset'
+    return render_template('public/forgot.html', title = title)
